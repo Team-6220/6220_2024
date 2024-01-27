e22a4666c7e085191e4ebf64b5b877c0832eda55
Samuel Li <lijinzesamuel@gmail.com>
2024-01-27 13:05:27 -0800

    Updated change speed based on how much angle off from apriltag

--- src/main/java/frc/robot/commands/LimelightAssistedSwerveCmd.java
+++ src/main/java/frc/robot/commands/LimelightAssistedSwerveCmd.java
@@ -26,6 +26,7 @@ public class LimelightAssistedSwerveCmd extends Command {
     limelightPidController = new PIDController(0.05,0.0,0.0);
     this.s_Swerve = s_Swerve;
     addRequirements(s_Swerve);
+    addRequirements(m_VisionSubsystem);
   }
 
   // Called when the command is initially scheduled.
@@ -37,16 +38,24 @@ public class LimelightAssistedSwerveCmd extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double steerOutput = limelightPidController.calculate(s_Swerve.getHeadingDegrees(),m_VisionSubsystem.getSteeringOffset()) * SwerveConstants.maxAngularVelocity;
-    steerOutput = (steerOutput > SwerveConstants.maxAngularVelocity) ? SwerveConstants.maxAngularVelocity : steerOutput;
+    // double steerOutput = limelightPidController.calculate(s_Swerve.getHeadingDegrees(),m_VisionSubsystem.getSteeringOffset()) * SwerveConstants.maxAngularVelocity;
+    // SmartDashboard.putNumber("BEFORE steeroutput", steerOutput);
+    // steerOutput = (steerOutput > SwerveConstants.maxAngularVelocity) ? SwerveConstants.maxAngularVelocity : (steerOutput < -SwerveConstants.maxAngularVelocity) ? -SwerveConstants.maxAngularVelocity : steerOutput;
+    double steerOutput = Normalization(m_VisionSubsystem.getSteeringOffset(), -180, 180, -1, 1) * SwerveConstants.maxAngularVelocity;
+    SmartDashboard.putNumber("AFTER steeroutput ", Normalization(m_VisionSubsystem.getSteeringOffset(), -180, 180, -1, 1));
     s_Swerve.drive(
             new Translation2d(0d, 0d), 
             // MathUtil.applyDeadband(limelightPidController.calculate(s_Swerve.getHeadingDegrees(),m_VisionSubsystem.getSteeringOffset()), 0), //NOT USABLE UPGRADED
             steerOutput,
             // (s_Swerve.getHeadingDegrees() - m_VisionSubsystem.getSteeringOffset()) * SwerveConstants.maxAngula15/rVelocity, //Original code from TeleopSwerve, NOT USABLE
             true, 
-            true
+            false
         );
+  }
+
+  public double Normalization(double v, double Min, double Max, double newMin, double newMax)
+  {
+    return (v-Min)/(Max-Min)*(newMax-newMin)+newMin;
   }
 
   // Called once the command ends or is interrupted.
