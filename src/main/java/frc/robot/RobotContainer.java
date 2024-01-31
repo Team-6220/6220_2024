599f264a3b4eb4057ddc3af2c547641d448fdc51
Sean Kelman <seanrkelman@gmail.com>
2024-01-30 21:40:16 -0800

    added armsubsystem and armtestcommand to robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,15 +1,19 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.ArmTestCommand;
 import frc.robot.commands.TeleopSwerve;
+import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.Swerve;
 
 public class RobotContainer {
   /* Controllers */
     private final XboxController driver = new XboxController(0);
+    private final Joystick js1 = new Joystick(1);
 
     /* Drive Controls */
     private final int translationAxis = XboxController.Axis.kLeftY.value;
@@ -22,18 +26,29 @@ public class RobotContainer {
 
     /* Subsystems */
     private final Swerve s_Swerve = new Swerve();
+    private final ArmSubsystem armSubsystem = ArmSubsystem.getInstance();
 
   public RobotContainer() {
     Constants.VisionConstants.setTagHeights();
 
     s_Swerve.setDefaultCommand(
         new TeleopSwerve(
-            s_Swerve, 
-            () -> -driver.getRawAxis(translationAxis), 
-            () -> -driver.getRawAxis(strafeAxis), 
-            () -> -driver.getRawAxis(rotationAxis), 
-            () -> robotCentric.getAsBoolean()
+          s_Swerve, 
+          () -> -driver.getRawAxis(translationAxis), 
+          () -> -driver.getRawAxis(strafeAxis), 
+          () -> -driver.getRawAxis(rotationAxis), 
+          () -> robotCentric.getAsBoolean()
         )
+    );
+
+    armSubsystem.setDefaultCommand(
+      new ArmTestCommand(
+        () -> driver.getAButton(),
+        () -> driver.getYButton(),
+        () -> driver.getLeftBumper(),
+        () -> driver.getRightBumper(),
+        () -> js1.getY()
+      )
     );
 
     configureButtonBindings();
