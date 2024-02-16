653f7adabc64b5ab219e65c006d756a886b23001
Samuel Li <lijinzesamuel@gmail.com>
2024-02-15 17:36:32 -0800

    Revert "Photonvision test code"
    This reverts commit cc4418edb6b1fb95d6132178defdfc4fdeb5f864.

--- src/main/java/frc/robot/subsystems/PhotonVisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/PhotonVisionSubsystem.java
@@ -105,12 +105,12 @@ public class PhotonVisionSubsystem extends SubsystemBase {
 
   public double getTurnOffset()
   {
-    if(!currResult.hasTargets())
+    if(hasTargets)
     {
-      return 0.0; 
+      SmartDashboard.putNumber("yaw", yaw);
+      return yaw;
     }
-    SmartDashboard.putNumber("yaw", currResult.getBestTarget().getYaw());
-    return currResult.getBestTarget().getYaw();
+    return -1.0; 
   }
 
   @Override
