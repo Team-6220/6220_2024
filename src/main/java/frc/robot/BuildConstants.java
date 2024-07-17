853e3dd933da5e26bc2d702e8e0e6247442cfe61
SamJinLi <lijinzesamuel@gmail.com>
2024-07-17 00:07:04 -0700

    Initialized Part of Advantage Kit
    *TODO: somehow get their swerve in our code or an equivalent alternative

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "6220_2024";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 166;
+  public static final String GIT_SHA = "a50316fab802b968626f4447a7a1d6299cbda9da";
+  public static final String GIT_DATE = "2024-07-16 21:55:49 PDT";
+  public static final String GIT_BRANCH = "Implementing_Advantage_Kit";
+  public static final String BUILD_DATE = "2024-07-17 00:06:12 PDT";
+  public static final long BUILD_UNIX_TIME = 1721199972867L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
