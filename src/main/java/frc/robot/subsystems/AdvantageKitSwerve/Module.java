518277f3829f21d5e3e060af944c81d8e37f8fb5
SamJinLi <lijinzesamuel@gmail.com>
2024-07-29 23:43:41 -0700

    Unfinished Module.java

---
+++ src/main/java/frc/robot/subsystems/AdvantageKitSwerve/Module.java
@@ -0,0 +1,13 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.AdvantageKitSwerve;
+
+/** Add your docs here. */
+public class Module
+{
+    private final ModuleIO io;
+    private final ModuleIOInputsAutoLogged inputs = new ModuleIOInputsAutoLogged();
+    private final int index;
+}
