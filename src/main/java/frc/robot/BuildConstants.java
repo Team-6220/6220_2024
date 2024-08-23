c2dda3f9a5ac9bcf26e12cbe3b15240ed5e999e1
SamJinLi <lijinzesamuel@gmail.com>
2024-08-23 15:11:18 -0700

    Update BuildConstants.java

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "6220_2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 180;
-  public static final String GIT_SHA = "571d86a3168528c858a37508484cab82addcbadc";
-  public static final String GIT_DATE = "2024-07-30 17:01:39 PDT";
+  public static final int GIT_REVISION = 181;
+  public static final String GIT_SHA = "a1d15736b534c53fb7c8bfbabe8c2bdce71462e4";
+  public static final String GIT_DATE = "2024-08-22 21:22:28 PDT";
   public static final String GIT_BRANCH = "Implementing_Advantage_Kit";
-  public static final String BUILD_DATE = "2024-08-22 21:22:05 PDT";
-  public static final long BUILD_UNIX_TIME = 1724386925331L;
+  public static final String BUILD_DATE = "2024-08-23 15:09:48 PDT";
+  public static final long BUILD_UNIX_TIME = 1724450988022L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
