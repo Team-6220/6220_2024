3d8eb8cb8657956662edb49245388254797bb6de
SamJinLi <lijinzesamuel@gmail.com>
2024-09-18 22:44:45 -0700

    Started implementing armio for sparkmax

--- src/main/java/frc/robot/subsystems/AdvantageKitArm/ArmIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/AdvantageKitArm/ArmIOSparkMax.java
@@ -1,21 +1,26 @@
 package frc.robot.subsystems.AdvantageKitArm;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import frc.robot.subsystems.AdvantageKitArm.ArmIO;
 import frc.lib.subsystem.AdvancedSubsystem;
+import frc.robot.Robot;
 import frc.robot.Constants.*;
 
 public class ArmIOSparkMax implements ArmIO {
-    private final CANSparkMax leftArmMotor, rightArmMotor;
+    private final CANSparkMax armMotorA = new CANSparkMax(ArmConstants.armMotorAID, MotorType.kBrushless);
+    private final CANSparkMax armMotorB = new CANSparkMax(ArmConstants.armMotorBID, MotorType.kBrushless);
+    
     private final DutyCycleEncoder armEncoder;
-
+    private final RelativeEncoder builtinEncoderA = armMotorA.getEncoder();
+    private final RelativeEncoder builtinEncoderB = armMotorB.getEncoder();
     public ArmIOSparkMax()
     {
-        leftArmMotor = new CANSparkMax(ArmConstants.armMotorAID, MotorType.kBrushless);
+        
     }
     
 
@@ -25,7 +30,9 @@ public class ArmIOSparkMax implements ArmIO {
    * @param inputs The inputs to update
    */
   @Override
-  void updateInputs(ArmInputs inputs){}
+  public void updateInputs(ArmInputs inputs)
+  {
+  }
 
   /**
    * Set the target angle for the arm joint
@@ -33,28 +40,36 @@ public class ArmIOSparkMax implements ArmIO {
    * @param angle Target arm angle
    */
   @Override
-  void setTargetAngle(Rotation2d angle){}
+  public void setTargetAngle(Rotation2d angle)
+  {}
 
   @Override
-  void setTargetAngle(Rotation2d angle, double maxVel, double maxAccel)
+  public void setTargetAngle(Rotation2d angle, double maxVel, double maxAccel)
+  {
+
+  }
 
   /**
    * Set the voltage output to the arm joint motors
    *
    * @param volts Voltage to output
    */
-  void setVoltage(double volts);
+  public void setVoltage(double volts)
+  {}
 
-  void brakeMotors();
+  public void brakeMotors()
+  {
+
+  }
 
   /**
    * Register self check compatible hardware with its associated subsystem
    *
    * @param subsystem The subsystem to register hardware on
    */
-  default void registerSelfCheckHardware(AdvancedSubsystem subsystem) {}
+  default public void registerSelfCheckHardware(AdvancedSubsystem subsystem) {}
 
   /** Optimize status signals for running sysID */
-  default void optimizeForSysID() {}
+  default public void optimizeForSysID() {}
 
 }
