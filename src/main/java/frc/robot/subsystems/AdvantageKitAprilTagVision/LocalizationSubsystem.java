b176dc752acf36e72d77e597e726b9abd3305d8c
SamJinLi <lijinzesamuel@gmail.com>
2024-10-09 10:36:58 -0700

    10/8/2024

--- src/main/java/frc/robot/subsystems/AdvantageKitAprilTagVision/LocalizationSubsystem.java
+++ src/main/java/frc/robot/subsystems/AdvantageKitAprilTagVision/LocalizationSubsystem.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LocalizationSubsystem extends SubsystemBase {
   /** Creates a new LocalizationSubsystem. */
-  AprilTagVisionIOArduCamPhotonIO[]
+   
   public LocalizationSubsystem() {}
 
   @Override
