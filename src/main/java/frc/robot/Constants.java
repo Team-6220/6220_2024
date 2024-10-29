9c9c888b18f0388418881826140462a53ff529a1
SamJinLi <lijinzesamuel@gmail.com>
2024-10-28 18:38:03 -0700

    Ok somehow our swerve module just automatically correct itself
    But the ports are right

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -515,9 +515,9 @@ public final class Constants {
         // Back Left Module 1
         public static final class Mod1 { //FIXME: This must be tuned to specific robot
             public static final int driveMotorID = 5;
-            public static final int angleMotorID = 11;
-            public static final int canCoderID = 2;
-            public static final Rotation2d angleOffset = Rotation2d.fromDegrees(120.14648437499999);
+            public static final int angleMotorID = 12;
+            public static final int canCoderID = 1;
+            public static final Rotation2d angleOffset = Rotation2d.fromDegrees(94.2);
             public static final SwerveModuleConstants constants = 
                 new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
         }
@@ -525,8 +525,8 @@ public final class Constants {
         //Front Right - Module 2
         public static final class Mod2 { //FIXME: This must be tuned to specific robot
             public static final int driveMotorID = 7;
-            public static final int angleMotorID = 12;
-            public static final int canCoderID = 1;
+            public static final int angleMotorID = 11;
+            public static final int canCoderID = 2;
             public static final Rotation2d angleOffset = Rotation2d.fromDegrees(93.779296875);
             public static final SwerveModuleConstants constants = 
                 new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
@@ -537,7 +537,7 @@ public final class Constants {
             public static final int driveMotorID = 6;
             public static final int angleMotorID = 9;
             public static final int canCoderID = 3;
-            public static final Rotation2d angleOffset = Rotation2d.fromDegrees(138.603515625);
+            public static final Rotation2d angleOffset = Rotation2d.fromDegrees(138.903515625);
             public static final SwerveModuleConstants constants = 
                 new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
         }
