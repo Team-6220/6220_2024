5d861f1eb9f21d9bda1f065795c459c5c0dc2788
SamJinLi <lijinzesamuel@gmail.com>
2024-11-16 13:55:03 -0800

    Made swerve work

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -518,7 +518,7 @@ public final class Constants {
         public static final class Mod1 { //FIXME: This must be tuned to specific robot
             public static final int driveMotorID = 5;
             public static final int angleMotorID = 12;
-            public static final int canCoderID = 1;
+            public static final int canCoderID = 2;
             public static final Rotation2d angleOffset = Rotation2d.fromDegrees(-148.8867);
             public static final SwerveModuleConstants constants = 
                 new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
@@ -528,11 +528,13 @@ public final class Constants {
         public static final class Mod2 { //FIXME: This must be tuned to specific robot
             public static final int driveMotorID = 7;
             public static final int angleMotorID = 11;
-            public static final int canCoderID = 2;
-            public static final Rotation2d angleOffset = Rotation2d.fromDegrees(93.779296875);
-            public static final SwerveModuleConstants constants = new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
+            public static final int canCoderID = 1;
+            public static final Rotation2d angleOffset = Rotation2d.fromDegrees(2.373046875);
+            public static final SwerveModuleConstants constants = 
+                new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
         }
 
+        //Front left Module 3
         public static final class Mod3 { //FIXME: This must be tuned to specific robot
             public static final int driveMotorID = 6;
             public static final int angleMotorID = 9;
